4831c70f4bf91867743a1c7a09f34e80c94e883f
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-07-18 19:38:16 +0530

    deployed

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     menuToggle.addEventListener('click', () => {
       navigation.classList.toggle('show');
     });
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwkFv3YuclPrxDkVsrR3uUwpJ283xoOO4aahRJTYfHTcv3Z-C0OA-YB-9HbaovfsoBQ/exec'
+            const form = document.forms['submit-to-google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => console.log('Success!', response))
+                .catch(error => console.error('Error!', error.message))
+            })
   });
   
