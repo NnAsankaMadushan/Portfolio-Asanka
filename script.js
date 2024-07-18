99c24327cc68255fdfad5b80b9342c13fb07d95e
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-07-18 19:22:42 +0530

    youtube

--- script.js
+++ script.js
@@ -5,22 +5,5 @@ document.addEventListener('DOMContentLoaded', () => {
     menuToggle.addEventListener('click', () => {
       navigation.classList.toggle('show');
     });
-
-    const scriptURL = "https://script.google.com/macros/s/AKfycbxYi8QFjY-70HwP2LDVWhkEyWvBjXXo-ylqqme4tqwmkdCoOcZC4WGCX6dzFnOTeyDJ/exec";
-            const form = document.forms['submit-to-google-sheet'];
-            const msg = document.getElementById("msg");
-          
-            form.addEventListener('submit', e => {
-              e.preventDefault();
-              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => {
-                  msg.innerHTML = "Message Sent Successfully!";
-                  setTimeout(function(){
-                    msg.innerHTML = "";
-                  }, 5000);
-                  form.reset();
-                })
-                .catch(error => console.error('Error!', error.message));
-            });
   });
   
